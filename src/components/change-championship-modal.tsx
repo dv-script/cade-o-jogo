190ac79ec1e750352a9b9e1615ab34c9b525d8c9
Davi Hasson Castro <davihasson@gmail.com>
2024-02-06 19:30:46 -0300

    feat: adding new change championship modal

---
+++ src/components/change-championship-modal.tsx
@@ -0,0 +1,68 @@
+"use client";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
+import { PiTrophy } from "react-icons/pi";
+import { IoFootball } from "react-icons/io5";
+import Link from "next/link";
+
+const championshipsOptions = [
+  { id: 1, label: "Paulistão Sicredi 2024", value: "paulistao-sicredi-2024" },
+  {
+    id: 2,
+    label: "Paulistão A2 Sicredi 2024",
+    value: "paulistao-a2-sicredi-2024",
+  },
+];
+
+export function ChangeChampionshipModal() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <Button onClick={onOpen} startContent={<PiTrophy />} variant="bordered">
+        Selecione<span className="hidden xs:block">o campeonato</span>
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        placement="center"
+        radius="lg"
+        backdrop="blur"
+        classNames={{
+          body: "py-6",
+          base: "border-[1px] border-[#292f46] bg-zinc-900 dark:bg-[#000] text-[#a8b0d3]",
+          header: "border-b-[1px] border-[#292f46]",
+          closeButton: "hover:bg-white/5 active:bg-white/10",
+        }}
+        size="lg"
+        scrollBehavior="outside"
+      >
+        <ModalContent>
+          <ModalHeader className="flex items-center gap-2">
+            <IoFootball />
+            <span>Selecione o campeonato:</span>
+          </ModalHeader>
+          <ModalBody>
+            {championshipsOptions.map((championship) => (
+              <Button
+                as={Link}
+                href={championship.value}
+                variant="bordered"
+                className="border-[#292f46] dark:border-[#292f46]"
+                key={championship.id}
+              >
+                {championship.label}
+              </Button>
+            ))}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
