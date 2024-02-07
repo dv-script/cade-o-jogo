fb914f0e5a1d5c883f8ff04703122dd59aa3e9bd
Davi Hasson Castro <davihasson@gmail.com>
2024-02-07 20:22:51 -0300

    feat: new async function to get matches from api

---
+++ src/data/get-matches.ts
@@ -0,0 +1,15 @@
+import { matchesURL } from "@/constants/footstats-url";
+import { unstable_noStore } from "next/cache";
+
+export async function getMatches() {
+  unstable_noStore();
+  try {
+    const response = await fetch(matchesURL, {
+      headers: { Authorization: `Bearer ${process.env.FOOTSTATS_TOKEN}` },
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.log("Error to get all matches", error);
+  }
+}
