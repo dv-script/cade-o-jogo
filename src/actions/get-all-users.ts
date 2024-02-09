e460e428570b3d95ddfb4c18ec1057d880612bdf
Davi Hasson Castro <davihasson@gmail.com>
2024-02-09 12:57:37 -0300

    feat: action to get all users from db

---
+++ src/actions/get-all-users.ts
@@ -0,0 +1,26 @@
+"use server";
+import { prisma } from "@/lib/prisma";
+
+export async function getAllUsers() {
+  try {
+    const users = await prisma.users.findMany({
+      select: {
+        id: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+        status: true,
+        roles: true,
+        createdAt: true,
+        updatedAt: true,
+        updatedBy: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return users;
+  } catch (error) {
+    return console.log(error);
+  }
+}
