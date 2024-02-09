d0622a26b6741a43136fc2f26eb4e764aff484f9
Davi Hasson Castro <davihasson@gmail.com>
2024-02-09 13:01:00 -0300

    feat: adding a new users page with a users table

---
+++ src/app/admin/dashboard/users/page.tsx
@@ -0,0 +1,24 @@
+import { UsersTable } from "@/app/admin/dashboard/users/_components/users-table";
+import { Metadata } from "next";
+import { IUser } from "@/types/IUser";
+import { getAllUsers } from "@/actions/get-all-users";
+
+export const metadata: Metadata = {
+  title: "FPF - User Management Dashboard",
+  description:
+    "Administrate and manage all user accounts with full control over creation, editing, and deletion. Access detailed user information and perform batch actions for efficient user management.",
+};
+
+export default async function Page() {
+  const users = (await getAllUsers()) as IUser[];
+
+  return (
+    <div className="flex p-4">
+      <div className="mx-auto flex flex-col gap-4 max-w-[1300px] overflow-hidden">
+        <div className="bg-zinc-900 flex flex-col rounded-lg p-3 w-full h-full">
+          <UsersTable users={users} />
+        </div>
+      </div>
+    </div>
+  );
+}
