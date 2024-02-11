2a727285380dc4af4344f52af655854cec2bc688
Davi Hasson Castro <davihasson@gmail.com>
2024-02-10 21:02:31 -0300

    chore: type from matches in db

---
+++ src/types/IMatchesFromDb.ts
@@ -0,0 +1,20 @@
+export interface IMatchesFromDb {
+  id: string;
+  idMatch: number;
+  hboMax: string;
+  canalTnt: string;
+  playPlus: string;
+  canalRecord: string;
+  cazeTv: string;
+  paulistaoPlay: string;
+  camisa21: string;
+  futebolPaulista: string;
+  canalFutura: string;
+  paulistao: string;
+  sportv: string;
+  globo: string;
+  premiere: string;
+  round: number;
+  teamHome: string;
+  teamAway: string;
+}
