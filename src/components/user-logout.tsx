f61702f0615d0cdb1fa2fa309215b291a6499b46
Davi Hasson Castro <davihasson@gmail.com>
2024-02-11 16:33:44 -0300

    style: translating the button to pt-br

--- src/components/user-logout.tsx
+++ src/components/user-logout.tsx
@@ -6,7 +6,7 @@ export function UserLogout() {
         <form action={logout} className="w-full">
             <button type="submit" className="w-full flex items-center gap-2 text-left rounded-lg px-4 py-2 transition hover:bg-red-500/25 hover:text-red-500">
                 <LuLogOut />
-                Sign Out
+                Sair
             </button>
         </form>
     )
